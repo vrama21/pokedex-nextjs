df0a55b2c3679fe25ee1a6aa3a7d534ae55d252e
Valon Rama <valon.rama@creditgenie.com>
2021-07-28 16:10:42 -0400

    Refactored getEvolutions to be a bit cleaner

--- api/getEvolutions.js
+++ api/getEvolutions.js
@@ -1,45 +1,42 @@
-const getEvolutionsList = async (evolutionChain, prevChain, pokedex, evolutions = [], evolutionStep = 0) => {
-  if (prevChain.length && evolutionStep >= prevChain.length) {
-    return evolutions;
-  }
-  let currentEvolutionStep = evolutionStep;
-
-  const pokemonName = evolutionChain.species.name;
-
-  const hasEvolutionDetails = evolutionChain.evolution_details.length > 0;
-  const hasEvolutions = prevChain.length > 0;
-  const hasMultipleEvolutions = prevChain.length > 1;
-
-  const evolutionDetails = hasEvolutionDetails
-    ? evolutionChain.evolution_details.map((evolutionDetail) => ({
-        trigger: evolutionDetail.trigger.name,
-        ...evolutionDetail,
-      }))
-    : [];
-
-  const pokemonData = await pokedex.getPokemonByName(pokemonName);
-
-  const evolution = {
-    name: pokemonName,
-    sprite: pokemonData.sprites.front_default,
-    evolutionDetails,
-  };
-
-  evolutions.push(evolution);
-
-  if (hasMultipleEvolutions && evolutionStep <= prevChain.length) {
-    currentEvolutionStep += 1;
-
-    return getEvolutionsList(prevChain[currentEvolutionStep], prevChain, pokedex, evolutions, evolutionStep);
-  }
+const getEvolutionsList = async (nextEvolution, pokedex, evolutions = []) => {
+  const prevPokemonName = nextEvolution.species.name;
+  const hasEvolutions = nextEvolution.evolves_to.length > 0;
 
   if (!hasEvolutions) {
     return evolutions;
   }
 
-  currentEvolutionStep = 0;
+  const evolutionChainPromises = nextEvolution.evolves_to.map(async (evolution) => {
+    const pokemonName = evolution.species.name;
+    const pokemonData = await pokedex.getPokemonByName(pokemonName);
 
-  return getEvolutionsList(evolutionChain.evolves_to[0], evolutionChain, pokedex, evolutions, evolutionStep);
+    const evolutionMethods = evolution.evolution_details.map((evolutionDetail) => {
+      const detailKeys = Object.keys(evolutionDetail).filter((detailKey) => evolutionDetail[detailKey] && detailKey !== 'trigger');
+      const requirements = detailKeys.map((key) => ({
+        [key]: evolutionDetail[key],
+      }));
+
+      return {
+        trigger: evolutionDetail.trigger.name,
+        requirements,
+      };
+    });
+
+    const evolutionModel = {
+      name: pokemonName,
+      sprite: pokemonData.sprites.front_default,
+      evolutionDetails: evolutionMethods,
+      evolvesFrom: prevPokemonName,
+    };
+
+    return evolutionModel;
+  });
+
+  const evolutionChain = await Promise.all(evolutionChainPromises);
+
+  evolutions.push(...evolutionChain);
+
+  return getEvolutionsList(nextEvolution.evolves_to[0], pokedex, evolutions);
 };
 
 export default getEvolutionsList;
--- components/Evolutions/evolutions.jsx
+++ components/Evolutions/evolutions.jsx
@@ -1,38 +1,53 @@
-import { capitalize } from 'lodash';
+import { capitalize, startCase } from 'lodash';
 import Layout from '../Layout/layout';
 
 const Evolutions = ({ evolutions }) => {
-  const evolutionChain = evolutions.map((evolution) => {
-    const hasEvolutionDetails = evolution.evolutionDetails.length > 0;
+  const hasEvolutions = evolutions.length > 0;
 
-    return (
-      <div key={evolution.name}>
-        <div>
-          {hasEvolutionDetails &&
-            evolution.evolutionDetails.map((evolutionDetail) => {
-              const trigger = evolutionDetail.trigger.name;
-              const method = trigger === 'level-up' && `Lv ${evolutionDetail.min_level}`;
-              console.log(evolutionDetail)
+  const evolutionChain =
+    hasEvolutions &&
+    evolutions.map((evolution) => {
+      const hasEvolutionDetails = evolution.evolutionDetails.length > 0;
 
-              return (
-                <span key={evolution.trigger}>
-                  <p>{method}</p>
-                </span>
-              );
-            })}
-        </div>
+      return (
         <div key={evolution.name}>
-          <img src={evolution.sprite} alt={evolution.name} />
-          <p className="text-center">{capitalize(evolution.name)}</p>
+          {hasEvolutionDetails && (
+            <div>
+              {evolution.evolutionDetails.map((evolutionDetail, evolutionDetailIndex) => {
+                const trigger = evolutionDetail.trigger.name;
+                const levelUp = trigger === 'level-up' && evolutionDetail.min_level && `(Lv ${evolutionDetail.min_level})`;
+                const levelUpTimeOfDay =
+                  trigger === 'level-up' && evolutionDetail.time_of_day && `Level at ${capitalize(evolutionDetail.time_of_day)}`;
+                const levelUpWithKnownMoveType =
+                  trigger === 'level-up' &&
+                  evolutionDetail.known_move_type &&
+                  `Level with a ${capitalize(evolutionDetail.known_move_type.name)} Move`;
+                const levelUpInSpecificLocation =
+                  trigger === 'level-up' && evolutionDetail.location && `Level at ${startCase(evolutionDetail.location.name.replace('-', ' '))}`;
+                const useItem = trigger === 'use-item' && startCase(evolutionDetail.item.name.replace('-', ' '));
+
+                const method = levelUp || levelUpTimeOfDay || levelUpWithKnownMoveType || levelUpInSpecificLocation || useItem;
+
+                return (
+                  <span key={evolutionDetailIndex}>
+                    <p className="text-center">{method}</p>
+                  </span>
+                );
+              })}
+            </div>
+          )}
+          <div key={evolution.name}>
+            <img className="mx-auto" src={evolution.sprite} alt={evolution.name} />
+            <p className="text-center">{capitalize(evolution.name)}</p>
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
 
   return (
     <Layout>
       <h2 className="text-center font-bold">Evolutions</h2>
-      <div className="flex justify-evenly">{evolutionChain}</div>
+      <div className="flex justify-evenly">{hasEvolutions ? evolutionChain : 'This Pokemon does not evolve'}</div>
     </Layout>
   );
 };
