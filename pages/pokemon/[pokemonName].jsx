60e6b590567e631f6b392488e3060546be6a0ad4
Valon Rama <valon.rama@creditgenie.com>
2021-08-01 14:45:29 -0400

    Deleted pokemonName directory and just made it a file. Also replaced getServerSideProps to getStaticPaths and getStaticProps for faster client side calls

---
+++ pages/pokemon/[pokemonName].jsx
@@ -0,0 +1,56 @@
+import Layout from 'components/Layout/layout';
+import PokedexData from 'components/PokedexData/PokedexData';
+import Moves from 'components/Moves/Moves';
+import Stats from 'components/Stats/Stats';
+import Evolutions from 'components/Evolutions/Evolutions';
+import getPokemon from 'api/getPokemon';
+import PokemonList from 'data/pokemonList.json';
+
+export const getStaticPaths = async () => {
+  const paths = PokemonList.map((pokemon) => {
+    return {
+      params: { pokemonName: pokemon.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  console.log(context);
+  const { pokemonName } = context.params;
+  const pokemonDataResponse = await getPokemon(pokemonName);
+
+  return {
+    props: {
+      pokemon: pokemonDataResponse,
+    },
+  };
+};
+
+export default function Pokemon({ pokemon }) {
+  return (
+    <>
+      <Layout>
+        <div>
+          <div className="flex justify-evenly">
+            <img className="w-1/2 px-4 py-2" src={pokemon.image} alt="logo" />
+            <div className="flex flex-wrap w-1/2">
+              <div className="w-full">
+                <PokedexData pokemon={pokemon} />
+              </div>
+              <div className="w-full">
+                <Stats stats={pokemon.stats} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+      <Evolutions evolutions={pokemon.evolutions} evolutionChain />
+      <Moves moves={pokemon.moves} />
+    </>
+  );
+}
