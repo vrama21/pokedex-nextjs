0f6e0520ad5cded0758e1b7ea1671e7339a8917c
Valon Rama <valon.rama@creditgenie.com>
2021-08-04 15:40:26 -0400

    Moves styling work is complete

--- components/Moves/Moves.jsx
+++ components/Moves/Moves.jsx
@@ -1,62 +1,26 @@
-import { startCase, upperCase } from 'lodash';
 import Layout from '../Layout/Layout';
-import Type from '../Type/Type';
 import LevelUpMoves from './LevelUpMoves/LevelUpMoves';
 import MachineMoves from './MachineMoves.jsx/MachineMoves';
+import EggMoves from './EggMoves/EggMoves';
+import TutorMoves from './TutorMoves/TutorMoves';
 
 const Moves = ({ moves, machines }) => {
-  const getMovesByMethod = (method) =>
-    moves
-      .filter((move) => move.method === method)
-      .map((move) => {
-        return (
-          <div className="flex justify-between px-2 py-1" key={move.name}>
-            <p className="font-bold text-sm w-full my-auto">{startCase(move.name).replace('-', ' ')}</p>
-            <Type type={move.type} />
-            {(move.method === 'level-up' || move.method === 'tutor') && (
-              <p className="text-sm text-right w-1/12 my-auto">{move.learnedAt === 0 ? 1 : move.learnedAt}</p>
-            )}
-          </div>
-        );
-      });
-
   const levelUpMoves = moves.filter((move) => move.method === 'level-up');
   const machineMoves = moves.filter((move) => move.method === 'machine');
-  const eggMoves = getMovesByMethod('egg');
-  const tutorMoves = getMovesByMethod('tutor');
+  const eggMoves = moves.filter((move) => move.method === 'egg');
+  const tutorMoves = moves.filter((move) => move.method === 'tutor');
 
   return (
     <Layout>
       <div className="flex justify-center">
-        {levelUpMoves.length > 0 && (
-          <div className="w-1/3 mx-2">
-            <LevelUpMoves moves={levelUpMoves} />
-          </div>
-        )}
-        {eggMoves.length > 0 && (
-          <div className="w-1/4 p-2 border-gray-300 border-2 rounded-md mx-2 h-full">
-            <h3 className="font-bold text-center">Moves learned by Breeding</h3>
-            <div className="flex justify-between px-2 font-bold border-gray-300 border-b-2 my-2">
-              <p>Name</p>
-            </div>
-            {eggMoves}
-          </div>
-        )}
-        {machineMoves.length > 0 && (
-          <div className="w-1/3 mx-2">
-            <MachineMoves machines={machines} moves={machineMoves} />
-          </div>
-        )}
-        {tutorMoves.length > 0 && (
-          <div className="w-1/4 p-2 border-gray-300 border-2 rounded-md mx-2 h-full">
-            <h3 className="font-bold text-center">Moves learned by Tutor</h3>
-            <div className="flex justify-between px-2 font-bold border-gray-300 border-b-2 my-2">
-              <p>Name</p>
-              <p>Level</p>
-            </div>
-            {tutorMoves}
-          </div>
-        )}
+        <div className="w-1/2 mx-2">
+          {levelUpMoves.length > 0 && <LevelUpMoves moves={levelUpMoves} />}
+          {eggMoves.length > 0 && <EggMoves moves={eggMoves} />}
+        </div>
+        <div className="w-1/2 mx-2">
+          {machineMoves.length > 0 && <MachineMoves machines={machines} moves={machineMoves} />}
+          {tutorMoves.length > 0 && <TutorMoves moves={tutorMoves} />}
+        </div>
       </div>
     </Layout>
   );
---
+++ components/Moves/TutorMoves/TutorMoves.jsx
@@ -0,0 +1,82 @@
+import { useMemo } from 'react';
+import { useTable, useSortBy } from 'react-table';
+import { startCase } from 'lodash';
+import Container from 'components/Container/Container';
+import Type from 'components/Type/Type';
+import styles from '../Moves.module.scss';
+
+const TutorMoves = ({ moves }) => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Type',
+        accessor: 'type',
+      },
+      {
+        Header: 'Category',
+        accessor: 'category',
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(
+    () =>
+      moves.map((move) => {
+        return {
+          category: move.category,
+          name: move.name,
+          type: move.type,
+        };
+      }),
+    [moves]
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data }, useSortBy);
+
+  const tableHeaders = headerGroups.map((headerGroup) => {
+    return (
+      <tr className={styles.tableHeader} key={headerGroup} {...headerGroup.getHeaderGroupProps()}>
+        {headerGroup.headers.map((column) => (
+          <th key={column} className="no-select" {...column.getHeaderProps(column.getSortByToggleProps())}>
+            {column.render('Header')}
+            {column.isSorted && <span>{column.isSortedDesc ? ' 🔽' : ' 🔼'}</span>}
+          </th>
+        ))}
+      </tr>
+    );
+  });
+
+  const tableRows = rows.map((row, rowIndex) => {
+    prepareRow(row);
+
+    return (
+      <tr className={styles.tableRow} key={rowIndex} {...row.getRowProps()}>
+        {row.cells.map((cell) => (
+          <td className={styles.moveCell} key={cell} {...cell.getCellProps()}>
+            {cell.column.Header === 'Name' && <span>{startCase(cell.value)}</span>}
+            {cell.column.Header === 'Type' && <Type type={cell.value} />}
+            {cell.column.Header === 'Category' && (
+              <img className={styles.moveCategory} src={`https://img.pokemondb.net/images/icons/move-${cell.value}.png`} alt={cell.value} />
+            )}
+          </td>
+        ))}
+      </tr>
+    );
+  });
+  return (
+    <Container>
+      <h3 className="font-bold text-center">Moves learned by Tutor</h3>
+      <table {...getTableProps}>
+        <thead>{tableHeaders}</thead>
+        <tbody {...getTableBodyProps}>{tableRows}</tbody>
+      </table>
+    </Container>
+  );
+};
+
+export default TutorMoves;
