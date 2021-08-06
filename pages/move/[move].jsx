b3dbd513f1d00b776c4e162cbb79e61c9ea3836b
Valon Rama <valon.rama@creditgenie.com>
2021-08-06 00:32:26 -0400

    Test

--- pages/move/[move].jsx
+++ pages/move/[move].jsx
@@ -3,40 +3,56 @@ import client from 'apollo-client';
 import { getMove, listAllMoves } from 'graphql/queries';
 import Layout from 'components/Layout/Layout';
 
-export const getStaticPaths = async () => {
-  const { data:
-    { pokemon_v2_move: moves }
-  } = await client.query({
-    query: gql(listAllMoves)
-  });
+// export const getStaticPaths = async () => {
+//   const {
+//     data: { pokemon_v2_move: moves },
+//   } = await client.query({
+//     query: gql(listAllMoves),
+//   });
 
-  const paths = moves.map((move) => {
-    return {
-      params: { move: move.name }
-    }
-  })
+//   const paths = moves.map((move) => {
+//     return {
+//       params: { move: move.name },
+//     };
+//   });
 
-  return {
-    paths,
-    fallback: true,
-  }
-}
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// };
 
-export const getStaticProps = async (context) => {
+// export const getStaticProps = async (context) => {
+//   const { move: moveName } = context.params;
+
+//   const {
+//     data: { pokemon_v2_move: moveData },
+//   } = await client.query({
+//     query: gql(getMove),
+//   });
+
+//   return {
+//     props: {
+//       move: moveData,
+//     },
+//   };
+// };
+
+export const getServerSideProps = async (context) => {
   const { move: moveName } = context.params;
 
-  const { data:
-    { pokemon_v2_move: moveData }
+  const {
+    data: { pokemon_v2_move: moveData },
   } = await client.query({
-    query: gql(getMove, { moveName: 'cut' })
+    query: gql(getMove),
   });
 
   return {
     props: {
       move: moveData,
-    }
-  }
-}
+    },
+  };
+};
 
 const Move = ({ move }) => {
   console.log(move);
@@ -44,7 +60,7 @@ const Move = ({ move }) => {
     <Layout>
       <p>{move}</p>
     </Layout>
-  )
-}
+  );
+};
 
 export default Move;
