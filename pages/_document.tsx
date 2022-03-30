a206167c0fa5e7b8a940e0419ba2915a2646d74c
Valon Rama <valon.rama@creditgenie.com>
2022-03-30 00:45:15 -0400

    Testing another font fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,22 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const MyDocument = () => {
-  async function getInitialProps(context: DocumentContext) {
-    const initialProps = await Document.getInitialProps(context)
-    return { ...initialProps }
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
-
-  return (
-    <Html>
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-
 }
 
 export default MyDocument
